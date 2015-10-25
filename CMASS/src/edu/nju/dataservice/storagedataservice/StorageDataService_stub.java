75ab632b71bea9b93e93a5eb1a430c275c028152
winddd <spmars4@gmail.com>
2015-10-25 17:59:37 +0800

    StorageDateService站桩
    StorageDateService站桩   对体系结构描述文档的一些修改

--- CMASS/src/edu/nju/dataservice/storagedataservice/StorageDataService_stub.java
+++ CMASS/src/edu/nju/dataservice/storagedataservice/StorageDataService_stub.java
@@ -1,5 +1,60 @@
 package edu.nju.dataservice.storagedataservice;
 
-public interface StorageDataService_stub {
+import java.util.ArrayList;
+
+import edu.nju.po.InWareHouseDocPO;
+import edu.nju.po.OutWareHouseDocPO;
+import edu.nju.po.RecordPO;
+
+public class StorageDataService_stub implements StorageDataService{
+	public void update(ArrayList<RecordPO> l){
+		System.out.println("Recordlist update success!");
+	}
+	
+	
+	public void clear(){
+		System.out.println("Record clear success!");
+	}
+	
+	
+	public ArrayList<RecordPO> getRecordList(){
+		ArrayList<RecordPO> t=new ArrayList<RecordPO>();
+		t.add(new RecordPO());//这边可以提供一些参数用来构造PO
+		t.add(new RecordPO());
+		return t;
+		}
+	
+	
+	public void modify(double t){
+		System.out.println("Modified!");
+	}
+	
+	
+	public void updateInWarehouseDoc(InWareHouseDocPO t){
+		System.out.println("updateInWarehouseDoc  success!");
+	}
+	
+	
+	public RecordPO find(int StorageItemID){
+		RecordPO p=new RecordPO();//需要加参数
+		return p;}
+	
+	
+	public double getPercent(String district){return 0.35;}
+	
+	
+	public ArrayList<OutWareHouseDocPO> getOutWarehouseDoc(){
+		ArrayList<OutWareHouseDocPO> t=new ArrayList<OutWareHouseDocPO>();
+		t.add(new OutWareHouseDocPO());//这边可以提供一些参数用来构造PO
+		t.add(new OutWareHouseDocPO());
+		return t;}//??
+	
+	
+	public ArrayList<InWareHouseDocPO> getInWarehouseDoc(){
+		ArrayList<InWareHouseDocPO> t=new ArrayList<InWareHouseDocPO>();
+		t.add(new InWareHouseDocPO());//这边可以提供一些参数用来构造PO
+		t.add(new InWareHouseDocPO());
+		return t;}
+
 
 }
