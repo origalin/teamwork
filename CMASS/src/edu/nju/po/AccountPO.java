18ed49166fe40420758695b5163fefcc91bff7c3
origalin <2360436350@qq.com>
2015-10-25 18:48:58 +0800

    haha

--- CMASS/src/edu/nju/po/AccountPO.java
+++ CMASS/src/edu/nju/po/AccountPO.java
@@ -1,5 +1,29 @@
 package edu.nju.po;
 
 public class AccountPO {
-
+	String name;
+	Double balance;
+	public AccountPO(String name, Double balance) {
+		super();
+		this.name = name;
+		this.balance = balance;
+	}
+	public AccountPO(String name) {
+		super();
+		this.name = name;
+		this.balance = 0.0;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getBalance() {
+		return balance;
+	}
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	
 }
