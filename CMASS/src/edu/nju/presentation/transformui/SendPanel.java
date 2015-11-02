e1c47a3a3de82321ee4d2bc959cf57e637c47c31
origalin <2360436350@qq.com>
2015-11-02 10:18:35 +0800

    加入派件界面

---
+++ CMASS/src/edu/nju/presentation/transformui/SendPanel.java
@@ -0,0 +1,133 @@
+package edu.nju.presentation.transformui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+@SuppressWarnings("serial")
+public class SendPanel extends JPanel{
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTable table;
+	public SendPanel() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{119, 1, 107, 93, 0, 0};
+		gridBagLayout.rowHeights = new int[]{41, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JButton btnNewButton = new JButton("\u786E\u8BA4\u6536\u4EF6");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.gridwidth = 5;
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 0;
+		add(panel_1, gbc_panel_1);
+		
+		JLabel label_3 = new JLabel("\u6D3E\u4EF6\u5355");
+		panel_1.add(label_3);
+		label_3.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridheight = 5;
+		gbc_scrollPane.gridwidth = 5;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 1;
+		add(scrollPane, gbc_scrollPane);
+		
+		JPanel panel = new JPanel();
+		scrollPane.setColumnHeaderView(panel);
+		
+		JLabel label = new JLabel("\u6D3E\u4EF6\u5355");
+		panel.add(label);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"单号", "收件人", "地址", "时间"
+			}
+		));
+		scrollPane.setViewportView(table);
+		
+		JButton button = new JButton("\u586B\u5199\u6536\u4EF6\u5355");
+		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.insets = new Insets(0, 0, 5, 0);
+		gbc_button.gridx = 4;
+		gbc_button.gridy = 6;
+		add(button, gbc_button);
+		
+		JLabel label_1 = new JLabel("\u6536\u4EF6\u7F16\u53F7");
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.insets = new Insets(0, 0, 5, 5);
+		gbc_label_1.gridx = 0;
+		gbc_label_1.gridy = 7;
+		add(label_1, gbc_label_1);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 2;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 7;
+		add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel label_2 = new JLabel("\u6536\u4EF6\u4EBA");
+		GridBagConstraints gbc_label_2 = new GridBagConstraints();
+		gbc_label_2.insets = new Insets(0, 0, 5, 5);
+		gbc_label_2.gridx = 3;
+		gbc_label_2.gridy = 7;
+		add(label_2, gbc_label_2);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 4;
+		gbc_textField_1.gridy = 7;
+		add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton.anchor = GridBagConstraints.NORTH;
+		gbc_btnNewButton.gridx = 4;
+		gbc_btnNewButton.gridy = 8;
+		add(btnNewButton, gbc_btnNewButton);
+		
+		JButton button_1 = new JButton("\u751F\u6210\u5230\u8FBE\u5355");
+		GridBagConstraints gbc_button_1 = new GridBagConstraints();
+		gbc_button_1.anchor = GridBagConstraints.NORTHWEST;
+		gbc_button_1.gridx = 4;
+		gbc_button_1.gridy = 9;
+		add(button_1, gbc_button_1);
+		
+
+	}
+}
