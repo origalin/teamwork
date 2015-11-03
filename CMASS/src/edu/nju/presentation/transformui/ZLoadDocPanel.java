dbe9f8cb568208080a6eb72902ba9fdbab507d45
origalin <2360436350@qq.com>
2015-11-03 18:51:58 +0800

    同

--- CMASS/src/edu/nju/presentation/transformui/ZLoadDocPanel.java
+++ CMASS/src/edu/nju/presentation/transformui/ZLoadDocPanel.java
@@ -20,34 +20,43 @@ import javax.swing.ScrollPaneConstants;
 import javax.swing.table.DefaultTableModel;
 
 public class ZLoadDocPanel extends JPanel{
-	private JTextField textField_3;
-	private JTextField textField_7;
-	private JTextField textField_8;
-	private JTextField textField_9;
-	private JTextField textField_10;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField transferCenterField;
+	private JTextField carIDField;
+	private JTextField watcherField;
+	private JTextField daliverField;
+	private JTextField targetField;
+	private JTextField itemIDField;
 	private JTable table;
 	public ZLoadDocPanel() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{418, 168, 0};
-		gridBagLayout.rowHeights = new int[]{248, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowHeights = new int[]{248, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		JPanel panel_11 = new JPanel();
 		GridBagConstraints gbc_panel_11 = new GridBagConstraints();
 		gbc_panel_11.fill = GridBagConstraints.BOTH;
-		gbc_panel_11.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_11.insets = new Insets(0, 0, 5, 5);
 		gbc_panel_11.gridx = 0;
 		gbc_panel_11.gridy = 0;
 		add(panel_11, gbc_panel_11);
 		panel_11.setBorder(null);
-		panel_11.setLayout(new GridLayout(6, 0, 0, 0));
+		GridBagLayout gbl_panel_11 = new GridBagLayout();
+		gbl_panel_11.columnWidths = new int[]{415, 0};
+		gbl_panel_11.rowHeights = new int[]{41, 41, 41, 41, 41, 0, 0};
+		gbl_panel_11.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel_11.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		panel_11.setLayout(gbl_panel_11);
 		
 		JPanel panel = new JPanel();
-		panel_11.add(panel);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		panel_11.add(panel, gbc_panel);
 		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
 		flowLayout.setAlignment(FlowLayout.LEFT);
 		
@@ -55,88 +64,103 @@ public class ZLoadDocPanel extends JPanel{
 		panel.add(label);
 		
 		JPanel panel_2 = new JPanel();
-		panel_11.add(panel_2);
+		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
+		gbc_panel_2.fill = GridBagConstraints.BOTH;
+		gbc_panel_2.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_2.gridx = 0;
+		gbc_panel_2.gridy = 1;
+		panel_11.add(panel_2, gbc_panel_2);
 		FlowLayout flowLayout_2 = (FlowLayout) panel_2.getLayout();
 		flowLayout_2.setAlignment(FlowLayout.LEFT);
 		
 		JLabel label_4 = new JLabel("\u4E2D\u8F6C\u4E2D\u5FC3\u7F16\u53F7");
 		panel_2.add(label_4);
 		
-		textField_3 = new JTextField();
-		panel_2.add(textField_3);
-		textField_3.setColumns(38);
-		
-		JPanel panel_5 = new JPanel();
-		panel_11.add(panel_5);
-		FlowLayout flowLayout_5 = (FlowLayout) panel_5.getLayout();
-		flowLayout_5.setAlignment(FlowLayout.LEFT);
-		
-		JLabel label_9 = new JLabel("\u6C7D\u8FD0\u7F16\u53F7  ");
-		panel_5.add(label_9);
-		
-		textField_7 = new JTextField();
-		textField_7.setColumns(40);
-		panel_5.add(textField_7);
+		transferCenterField = new JTextField();
+		panel_2.add(transferCenterField);
+		transferCenterField.setColumns(38);
 		
 		JPanel panel_6 = new JPanel();
-		panel_11.add(panel_6);
+		GridBagConstraints gbc_panel_6 = new GridBagConstraints();
+		gbc_panel_6.fill = GridBagConstraints.BOTH;
+		gbc_panel_6.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_6.gridx = 0;
+		gbc_panel_6.gridy = 2;
+		panel_11.add(panel_6, gbc_panel_6);
 		FlowLayout flowLayout_6 = (FlowLayout) panel_6.getLayout();
 		flowLayout_6.setAlignment(FlowLayout.LEFT);
 		
-		JLabel label_10 = new JLabel("\u5230\u8FBE\u5730    ");
+		JLabel label_10 = new JLabel("\u5230\u8FBE\u5730      ");
 		panel_6.add(label_10);
 		
-		textField = new JTextField();
-		panel_6.add(textField);
-		textField.setColumns(20);
+		targetField = new JTextField();
+		panel_6.add(targetField);
+		targetField.setColumns(20);
 		
 		JPanel panel_8 = new JPanel();
-		panel_11.add(panel_8);
+		GridBagConstraints gbc_panel_8 = new GridBagConstraints();
+		gbc_panel_8.fill = GridBagConstraints.BOTH;
+		gbc_panel_8.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_8.gridx = 0;
+		gbc_panel_8.gridy = 3;
+		panel_11.add(panel_8, gbc_panel_8);
 		FlowLayout flowLayout_7 = (FlowLayout) panel_8.getLayout();
 		flowLayout_7.setAlignment(FlowLayout.LEFT);
 		
-		JLabel label_11 = new JLabel("\u8F66\u8F86\u4EE3\u53F7  ");
+		JLabel label_11 = new JLabel("\u8F66\u8F86\u4EE3\u53F7    ");
 		panel_8.add(label_11);
 		
-		textField_8 = new JTextField();
-		panel_8.add(textField_8);
-		textField_8.setColumns(5);
+		carIDField = new JTextField();
+		panel_8.add(carIDField);
+		carIDField.setColumns(5);
 		
 		JLabel lblNewLabel = new JLabel("\u76D1\u88C5\u5458");
 		panel_8.add(lblNewLabel);
 		
-		textField_9 = new JTextField();
-		panel_8.add(textField_9);
-		textField_9.setColumns(5);
+		watcherField = new JTextField();
+		panel_8.add(watcherField);
+		watcherField.setColumns(5);
 		
 		JLabel label_12 = new JLabel("\u62BC\u8FD0\u5458");
 		panel_8.add(label_12);
 		
-		textField_10 = new JTextField();
-		panel_8.add(textField_10);
-		textField_10.setColumns(5);
+		daliverField = new JTextField();
+		panel_8.add(daliverField);
+		daliverField.setColumns(5);
 		
 		JPanel panel_12 = new JPanel();
 		FlowLayout flowLayout_12 = (FlowLayout) panel_12.getLayout();
 		flowLayout_12.setAlignment(FlowLayout.RIGHT);
-		panel_11.add(panel_12);
-		
-		JButton button_1 = new JButton("\u751F\u6210\u4E2D\u8F6C\u4E2D\u5FC3\u88C5\u8F66\u5355");
-		panel_12.add(button_1);
+		GridBagConstraints gbc_panel_12 = new GridBagConstraints();
+		gbc_panel_12.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_12.fill = GridBagConstraints.BOTH;
+		gbc_panel_12.gridx = 0;
+		gbc_panel_12.gridy = 4;
+		panel_11.add(panel_12, gbc_panel_12);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setLayout(null);
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
 		gbc_panel_1.fill = GridBagConstraints.BOTH;
 		gbc_panel_1.gridx = 1;
 		gbc_panel_1.gridy = 0;
 		add(panel_1, gbc_panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{168, 0};
+		gbl_panel_1.rowHeights = new int[]{170, 24, 27, 0};
+		gbl_panel_1.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(0, 0, 168, 170);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		panel_1.add(scrollPane);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 0;
+		panel_1.add(scrollPane, gbc_scrollPane);
 		
 		JPanel panel_13 = new JPanel();
 		scrollPane.setColumnHeaderView(panel_13);
@@ -161,22 +185,35 @@ public class ZLoadDocPanel extends JPanel{
 		table.getColumnModel().getColumn(0).setPreferredWidth(130);
 		panel_14.add(table);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(15);
-		textField_1.setBounds(14, 183, 126, 24);
-		panel_1.add(textField_1);
+		itemIDField = new JTextField();
+		itemIDField.setColumns(30);
+		GridBagConstraints gbc_itemIDField = new GridBagConstraints();
+		gbc_itemIDField.anchor = GridBagConstraints.NORTH;
+		gbc_itemIDField.insets = new Insets(0, 0, 5, 0);
+		gbc_itemIDField.gridx = 0;
+		gbc_itemIDField.gridy = 1;
+		panel_1.add(itemIDField, gbc_itemIDField);
 		
-		JButton button = new JButton("\u6DFB\u52A0");
-		button.setBounds(49, 211, 63, 27);
-		button.addActionListener(new ActionListener() {
+		JButton addButton = new JButton("\u6DFB\u52A0");
+		addButton.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO 自动生成的方法存根
-				tableModel.addRow(new Object[] {textField_1.getText()});
+				tableModel.addRow(new Object[] {itemIDField.getText()});
 			}
 		});
-		panel_1.add(button);
+		GridBagConstraints gbc_addButton = new GridBagConstraints();
+		gbc_addButton.anchor = GridBagConstraints.NORTH;
+		gbc_addButton.gridx = 0;
+		gbc_addButton.gridy = 2;
+		panel_1.add(addButton, gbc_addButton);
+		
+		JButton createButton = new JButton("\u751F\u6210\u4E2D\u8F6C\u4E2D\u5FC3\u88C5\u8F66\u5355");
+		GridBagConstraints gbc_createButton = new GridBagConstraints();
+		gbc_createButton.gridx = 1;
+		gbc_createButton.gridy = 1;
+		add(createButton, gbc_createButton);
 		
 
 	}
