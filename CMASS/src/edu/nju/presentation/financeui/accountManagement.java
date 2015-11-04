dce69a9868870d38bf1131e42c0623c49143f16c
lizhimu <2748738706@qq.com>
2015-11-04 16:18:23 +0800

    补上了账户的增删改查

--- CMASS/src/edu/nju/presentation/financeui/accountManagement.java
+++ CMASS/src/edu/nju/presentation/financeui/accountManagement.java
@@ -1,5 +1,101 @@
 package edu.nju.presentation.financeui;
 import javax.swing.*;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Font;
 public class accountManagement extends JPanel{
+	private JTextField textField;
+	private JTable table;
+	public accountManagement() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 302, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 140, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel label = new JLabel("\u8D26\u6237\u7BA1\u7406");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.insets = new Insets(0, 0, 5, 5);
+		gbc_label.gridx = 0;
+		gbc_label.gridy = 0;
+		add(label, gbc_label);
+		
+		JButton button = new JButton("\u589E\u52A0");
+		button.setFont(new Font("黑体", Font.BOLD, 12));
+		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.insets = new Insets(0, 0, 5, 5);
+		gbc_button.gridx = 1;
+		gbc_button.gridy = 1;
+		add(button, gbc_button);
+		
+		JButton button_1 = new JButton("\u5220\u9664");
+		button_1.setFont(new Font("黑体", Font.BOLD, 12));
+		GridBagConstraints gbc_button_1 = new GridBagConstraints();
+		gbc_button_1.insets = new Insets(0, 0, 5, 5);
+		gbc_button_1.gridx = 2;
+		gbc_button_1.gridy = 1;
+		add(button_1, gbc_button_1);
+		
+		JButton button_2 = new JButton("\u4FEE\u6539");
+		button_2.setFont(new Font("黑体", Font.BOLD, 12));
+		GridBagConstraints gbc_button_2 = new GridBagConstraints();
+		gbc_button_2.insets = new Insets(0, 0, 5, 5);
+		gbc_button_2.gridx = 1;
+		gbc_button_2.gridy = 2;
+		add(button_2, gbc_button_2);
+		
+		JButton button_3 = new JButton("\u67E5\u770B");
+		button_3.setFont(new Font("黑体", Font.BOLD, 12));
+		GridBagConstraints gbc_button_3 = new GridBagConstraints();
+		gbc_button_3.insets = new Insets(0, 0, 5, 5);
+		gbc_button_3.gridx = 2;
+		gbc_button_3.gridy = 2;
+		add(button_3, gbc_button_3);
+		
+		JLabel label_1 = new JLabel("\u8D26\u6237\u540D\u79F0");
+		label_1.setFont(new Font("黑体", Font.BOLD, 12));
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.insets = new Insets(0, 0, 5, 5);
+		gbc_label_1.anchor = GridBagConstraints.EAST;
+		gbc_label_1.gridx = 1;
+		gbc_label_1.gridy = 4;
+		add(label_1, gbc_label_1);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 4;
+		add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane.gridx = 2;
+		gbc_scrollPane.gridy = 6;
+		add(scrollPane, gbc_scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+			},
+			new String[] {
+				"\u516C\u53F8\u94F6\u884C\u8D26\u6237"
+			}
+		));
+	}
 
 }
