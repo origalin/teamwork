a1cd6d7a5231dffa878272effa3caa919125114b
origalin <2360436350@qq.com>
2015-11-06 20:24:03 +0800

    补全2

--- CMASS/src/edu/nju/businesslogicservice/transformlogicservice/YLoadDocService.java
+++ CMASS/src/edu/nju/businesslogicservice/transformlogicservice/YLoadDocService.java
@@ -5,6 +5,7 @@ import edu.nju.vo.YLoadDocVO;
 
 public interface YLoadDocService {
 	public void saveYloadDocPO(YLoadDocPO po);//保存营业厅装车单持久化对象
+	public int[] getSendDocIDList(int id);
 	public int getYloadSequence();//获取装车单基准序列号
 	public void changeYloadSequence(int YloadSequence);//改变基准序列号
 	public YLoadDocVO createYLoadDocVO(int carID,String target,String watcher,String deliver);//创建装车单VO
--- CMASS/src/edu/nju/businesslogicservice/transformlogicservice/ZLoadDocService.java
+++ CMASS/src/edu/nju/businesslogicservice/transformlogicservice/ZLoadDocService.java
@@ -6,6 +6,7 @@ import edu.nju.vo.ZLoadDocVO;
 public interface ZLoadDocService {
 	public void saveZloadDocPO(ZLoadDocPO po);//保存中转中心装车单持久化对象
 	public int getZloadSequence();//获取装车单基准序列号
+	public int[] getSendDocIDList(int id);
 	public void changeZloadSequence(int ZloadSequence);//改变基准序列号
 	public ZLoadDocVO createZLoadDocVO(int carID,String target,String watcher,String deliver);//创建装车单VO
 	public ZLoadDocVO findZLoadDocVO(int ID);//获取装车单VO/
