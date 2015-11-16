5318c1e16576e4c7fca7d33cfb8fb992e59f807d
origalin <2360436350@qq.com>
2015-11-16 13:47:27 +0800

    同步‘

--- CMASS/src/edu/nju/businesslogic/collectionbl/MoocCollectionbl.java
+++ CMASS/src/edu/nju/businesslogic/collectionbl/MoocCollectionbl.java
@@ -1,7 +1,15 @@
 package edu.nju.businesslogic.collectionbl;
 
+import java.util.ArrayList;
+
+import edu.nju.po.SendDocPO;
+
 public class MoocCollectionbl extends Collectionbl{
 	String getTargetAddress(String itemID) {
 		return "栖霞区仙林大道168号";
+	}
+	ArrayList<SendDocPO> getSendDocPOsPyID(String staffID){
+		return null;
+		
 	}
 }
