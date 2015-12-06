e3d0377204fb9566df905f6ef0897fdb117dd108
LPT <lpt14@software.nju.edu.cn>
2015-12-06 14:35:27 +0800

    加入数据库脚本

---
+++ mysql脚本/mysql/distance.sql
@@ -0,0 +1,3 @@
+INSERT INTO `distance` (`id`, `city1`, `city2`, `distance`) VALUES (1, '南京', '北京', 600);
+INSERT INTO `distance` (`id`, `city1`, `city2`, `distance`) VALUES (2, '深圳', '北京', 5000);
+INSERT INTO `distance` (`id`, `city1`, `city2`, `distance`) VALUES (3, '上海', '北京', 1000);
---
+++ mysql脚本/mysql/password.sql
@@ -0,0 +1,1 @@
+INSERT INTO `password` (`id`, `password`, `power`) VALUES ('00250001000', '123456', '总经理');
