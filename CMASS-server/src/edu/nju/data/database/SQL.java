35aea8ffb673fb9eab607bcebdc4d4f978c5ed08
origalin <2360436350@qq.com>
2016-01-05 10:23:13 +0800

    ip

--- CMASS-server/src/edu/nju/data/database/SQL.java
+++ CMASS-server/src/edu/nju/data/database/SQL.java
@@ -17,7 +17,7 @@ public class SQL {
 	public static Statement stmt = null;
 	public static ResultSet rs = null;
 
-	public static String url ="jdbc:mysql://114.212.42.46:3306/cmass";	
+	public static String url ="jdbc:mysql://192.168.191.8:3306/cmass";	
 
 	public static String user = "root";
 	public static String password = "1314..zJ";
